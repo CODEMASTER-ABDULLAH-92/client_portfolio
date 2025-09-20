8cd6b717a81e4c9e13ea2d5ca196f1171b36b0db
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-09-20 08:57:45 +0500

    Update route.js

--- src/app/api/quote/route.js
+++ src/app/api/quote/route.js
@@ -30,7 +30,7 @@ export async function POST(req) {
 
     // ✅ Send confirmation email to user
     await transporter.sendMail({
-      from: `"Waseem Riaz | Web Developer" <>`,
+      from: `"Waseem Riaz | Web Developer" <${process.env.EMAIL_USER}>`,
       to: email,
       subject: "✅ We received your message",
       text: `Hi ${name},
